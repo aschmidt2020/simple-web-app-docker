8cc5bdc8a093281409777a13ec557681295f1fd7
Audrey Schmidt <audsch@outlook.com>
2022-01-28 13:53:49 -0600

    added volumes to docker-compose

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => { //request and response
-    res.send('Hi there dCC class - ASchmidt');
+    res.send('Hi there dCC class - Audrey Schmidt');
 });
 
 app.listen(3000, () => {
